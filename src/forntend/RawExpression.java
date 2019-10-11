2cd20648ee591ae9e11165be98bc28705a6976e6
Kamal (Riwogo) <pmer93@gmail.com>
2019-10-11 14:00:28 +0330

    add RawExpression class

---
+++ src/forntend/RawExpression.java
@@ -0,0 +1,24 @@
+package forntend;
+
+public class RawExpression {
+	final public String word;
+	final public RawExpression[] expressions;
+	
+	public RawExpression(final String word, final RawExpression[] expressions) {
+		this.word = word;
+		this.expressions = expressions;
+	}
+	
+	public String toString() {
+		if (this.expressions.length == 0) {
+			return this.word;
+		} else {
+			String expsStr = "";
+			for (int i = 0; i < this.expressions.length; i++) {
+				expsStr += " " + this.expressions[i].toString();
+			}
+			
+			return "(" + this.word + expsStr + ")";
+		}
+	}
+}
